f6291e7ebc5a0e6478e801f3a3cbd42defaa327e
5751mn <edi.eastfield99@gmail.com>
2022-01-28 01:49:20 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/myforum/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.myforum.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.myforum.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/dicoding/myforum/register/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.dicoding.myforum.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.myforum.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
