60000d1c6d4133a673985a4cf345fae280146656
5751mn <edi.eastfield99@gmail.com>
2022-01-28 15:42:58 +0800

    update login activity

--- app/src/main/java/com/dicoding/myforum/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/myforum/login/LoginActivity.kt
@@ -2,11 +2,32 @@ package com.dicoding.myforum.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.dicoding.myforum.R
+import com.dicoding.myforum.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener(this)
+        binding.tvCreateAccount.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btn_login-> {
+                Toast.makeText(this,"Login",Toast.LENGTH_SHORT).show()
+            }
+            R.id.tv_createAccount-> {
+                Toast.makeText(this,"Create Account",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
